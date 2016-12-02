37cfb21380c48148f83edf9999e19236a31eda4a
dtbhuyen <huyen.duong.124@gmail.com>
2016-12-02 15:07:18 +0700

    Initial Project

---
+++ Bai10/Bai10/frmOrder.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Bai10
+{
+    partial class frmOrder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboOrderID = new System.Windows.Forms.ComboBox();
+            this.txtEmployeeID = new System.Windows.Forms.TextBox();
+            this.txtCustomerID = new System.Windows.Forms.TextBox();
+            this.dtpOrderDate = new System.Windows.Forms.DateTimePicker();
+            this.dtpShippedDate = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgvOrderDetail = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btluu = new System.Windows.Forms.Button();
+            this.bthuy = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrderDetail)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboOrderID
+            // 
+            this.cboOrderID.FormattingEnabled = true;
+            this.cboOrderID.Location = new System.Drawing.Point(99, 32);
+            this.cboOrderID.Name = "cboOrderID";
+            this.cboOrderID.Size = new System.Drawing.Size(121, 21);
+            this.cboOrderID.TabIndex = 0;
+            this.cboOrderID.SelectedIndexChanged += new System.EventHandler(this.cboOrderID_SelectedIndexChanged);
+            // 
+            // txtEmployeeID
+            // 
+            this.txtEmployeeID.Location = new System.Drawing.Point(408, 69);
+            this.txtEmployeeID.Name = "txtEmployeeID";
+            this.txtEmployeeID.Size = new System.Drawing.Size(133, 20);
+            this.txtEmployeeID.TabIndex = 1;
+            // 
+            // txtCustomerID
+            // 
+            this.txtCustomerID.Location = new System.Drawing.Point(99, 69);
+            this.txtCustomerID.Name = "txtCustomerID";
+            this.txtCustomerID.Size = new System.Drawing.Size(129, 20);
+            this.txtCustomerID.TabIndex = 2;
+            // 
+            // dtpOrderDate
+            // 
+            this.dtpOrderDate.Location = new System.Drawing.Point(129, 104);
+            this.dtpOrderDate.Name = "dtpOrderDate";
+            this.dtpOrderDate.Size = new System.Drawing.Size(179, 20);
+            this.dtpOrderDate.TabIndex = 3;
+            // 
+            // dtpShippedDate
+            // 
+            this.dtpShippedDate.Location = new System.Drawing.Point(449, 104);
+            this.dtpShippedDate.Name = "dtpShippedDate";
+            this.dtpShippedDate.Size = new System.Drawing.Size(187, 20);
+            this.dtpShippedDate.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.dtpOrderDate);
+            this.groupBox1.Controls.Add(this.txtEmployeeID);
+            this.groupBox1.Controls.Add(this.txtCustomerID);
+            this.groupBox1.Controls.Add(this.dtpShippedDate);
+            this.groupBox1.Controls.Add(this.cboOrderID);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(642, 143);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Hóa đơn";
+            // 
+            // dgvOrderDetail
+            // 
+            this.dgvOrderDetail.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOrderDetail.Location = new System.Drawing.Point(12, 169);
+            this.dgvOrderDetail.Name = "dgvOrderDetail";
+            this.dgvOrderDetail.Size = new System.Drawing.Size(642, 226);
+            this.dgvOrderDetail.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ma HD";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "MaNV";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ngay lap HD";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(347, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Ma KH";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(347, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Ngay giao hang";
+            // 
+            // btluu
+            // 
+            this.btluu.AutoEllipsis = true;
+            this.btluu.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btluu.Location = new System.Drawing.Point(233, 402);
+            this.btluu.Name = "btluu";
+            this.btluu.Size = new System.Drawing.Size(75, 23);
+            this.btluu.TabIndex = 6;
+            this.btluu.Text = "Lưu";
+            this.btluu.UseVisualStyleBackColor = true;
+            // 
+            // bthuy
+            // 
+            this.bthuy.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bthuy.Location = new System.Drawing.Point(343, 402);
+            this.bthuy.Name = "bthuy";
+            this.bthuy.Size = new System.Drawing.Size(75, 23);
+            this.bthuy.TabIndex = 6;
+            this.bthuy.Text = "Hủy";
+            this.bthuy.UseVisualStyleBackColor = true;
+            // 
+            // frmOrder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 437);
+            this.Controls.Add(this.bthuy);
+            this.Controls.Add(this.btluu);
+            this.Controls.Add(this.dgvOrderDetail);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmOrder";
+            this.Text = "frmOrder";
+            this.Load += new System.EventHandler(this.frmOrder_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrderDetail)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboOrderID;
+        private System.Windows.Forms.TextBox txtEmployeeID;
+        private System.Windows.Forms.TextBox txtCustomerID;
+        private System.Windows.Forms.DateTimePicker dtpOrderDate;
+        private System.Windows.Forms.DateTimePicker dtpShippedDate;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvOrderDetail;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btluu;
+        private System.Windows.Forms.Button bthuy;
+    }
+}
---
+++ Bai10/Bai10/frmOrder.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace Bai10
+{
+    public partial class frmOrder : Form
+    {
+        string cnStr = "";
+        SqlConnection cn;
+        DataSet ds;
+        DataTable Orders;
+        public frmOrder()
+        {
+            InitializeComponent();
+        }
+
+        private void frmOrder_Load(object sender, EventArgs e)
+        {
+            cnStr = ConfigurationManager.ConnectionStrings["cnStr"].ConnectionString;
+            cn = new SqlConnection(cnStr);
+
+            //string sql = "SELECT * FROM CTHD";
+            //dgvOrderDetail.DataSource = GetCustomerDataset().Tables[0];
+
+            string sql = "SELECT * FROM HoaDon";
+            Orders = GetCustomerDataset(sql).Tables[0];
+
+            //Binding du lieu cho ComboBox
+            cboOrderID.DataSource = Orders;
+            cboOrderID.DisplayMember = "MaHD";
+            cboOrderID.ValueMember = "MaHD";
+
+            txtEmployeeID.DataBindings.Add("Text", Orders, "MaNV");
+            txtCustomerID.DataBindings.Add("Text", Orders, "MaKH");
+
+            dtpOrderDate.DataBindings.Add("Text", Orders, "NgayLapHD");
+            dtpShippedDate.DataBindings.Add("Text", Orders, "NgayGiaoHang");
+        }
+        public DataSet GetCustomerDataset(string sql)
+        {
+            try
+            {
+                //sql = "SELECT * FROM CTHD";
+                SqlDataAdapter da = new SqlDataAdapter(sql, cn);
+                /*DataSet*/
+                ds = new DataSet();
+                da.Fill(ds);
+                return ds;
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        private void cboOrderID_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string sql = "SELECT * FROM CTHD WHERE MaHD = '" + cboOrderID.Text + "'";
+            dgvOrderDetail.DataSource = GetCustomerDataset(sql).Tables[0];
+        }
+    }
+}
